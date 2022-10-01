a705b016040dda4e2ffaa5e2f8936519fe9e8ac8
Quentin Bonnet <quentin.bonnet.work@gmail.com>
2022-10-01 14:46:33 +0200

    ir-decoder project is about finding out what are the button codes of your NEC remote

--- example-projects/ir-decoder/main.cpp
+++ example-projects/ir-decoder/main.cpp
@@ -1,10 +1,19 @@
+/*
+ * Get the button codes from your NEC protocol remote.
+ *
+ */
 #include "pico/stdlib.h"
 #include <iostream>
 
-#include "pico-cpp-lib/ir-remotes/TDJL20KEYS.h"
+#include "pico-cpp-lib/ir-remotes/protocols/NEC.h"
 
 const uint ALIM_PIN = 28;
 const uint INFRARED_RECEIVER_GPIO_PIN = 26;
+
+// TODO extract in a front lib header containing this and the debug utils...
+// Or put it in a cpp built and linked, no need for header? 
+std::ostream& operator<<(std::ostream& s, uint8_t n) { return s << (uint) n; }
+std::ostream& operator<<(std::ostream& s, uint16_t n) { return s << (uint) n; }
 
 int main() {
     // Necessary to get IO from USB, and reboot without using the physical boot button
@@ -14,13 +23,15 @@ int main() {
     gpio_set_dir(ALIM_PIN, GPIO_OUT);
     gpio_put(ALIM_PIN, 1);
 
-    ir::Tdjl20Keys remote(INFRARED_RECEIVER_GPIO_PIN);
+    ir::nec::initOnGpio(INFRARED_RECEIVER_GPIO_PIN);
+
+    IrqFifo<ir::nec::DataFrame>& remoteFifo = ir::nec::outFifo();
 
     while(true) {
-        if (remote.hasEvent()) {
-            ir::Tdjl20Keys::ButtonEvent event = remote.nextEvent();
-            std::cout << "Button [" << ir::Tdjl20Keys::buttonStr(event.button) << "] was pressed ";
-            std::cout << (time_us_64() - event.time) / 1000 << " ms ago" << std::endl;
+        if (remoteFifo.hasMessages()) {
+            ir::nec::DataFrame data = remoteFifo.pop();
+            std::cout << "NEC data frame received [Address: " << data.address << ", Command: " << data.command << "] ";
+            std::cout << (time_us_64() - data.startTime) / 1000 << " ms ago" << std::endl;
         }
     }
 }
